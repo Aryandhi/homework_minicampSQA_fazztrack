3472b1869accc80171670d0375bb96dd9e6e6a19
aryandhi.a.w <aryandh.a.w@gmail.com>
2022-08-08 15:26:09 +0700

    simple testcase with mocha

--- sesi5_apiAutomation/api/user-endpoint.js
+++ sesi5_apiAutomation/api/user-endpoint.js
@@ -1,8 +1,29 @@
 const httpCaller = require('supertest');
 const URL = httpCaller('http://localhost:1234');
 
-function getUser() {
-  return URL.get('/v1/users')
+//search user use parameter statis
+function getUserStatic() {
+  return URL.get('/v1/users?name=ramadhan')
     .set('Connection', 'keep-alive')
-    .set('Content-type', 'application/json');
+    .set('Content-type', 'application/json')
 }
+
+// search user use params dinamis
+function getUser(inputName) {
+  return URL.get(`/v1/users?name=${inputName}`)
+    .set('Connection', 'keep-alive')
+    .set('Content-type', 'application/json')
+}
+
+function postUser(bodyRequest) {
+  return URL.post(`/v1/users`)
+    .set('Connection', 'keep-alive')
+    .set('Content-type', 'application/json')
+    .send(bodyRequest)
+}
+
+module.exports = {
+  getUserStatic,
+  getUser,
+  postUser,
+};
--- sesi5_apiAutomation/testcases/user-testcase.js
+++ sesi5_apiAutomation/testcases/user-testcase.js
@@ -1,7 +1,27 @@
 const api = require('../api/user-endpoint');
 
-describe('GET ALL /v1/users', async()=> {
+describe('GET By Name, /v1/users', async()=> {
   it('WHEN searching data un-registered', async()=> {
-    
+    let responseApi = await api.getUser('ridho');
+    console.log("status code: ", responseApi.status);
+    console.log(responseApi.body);
+  })
+
+  it('WHEN search user registered with static way', async()=> {
+    responseApi = await api.getUserStatic()
+    console.log("status code: ", responseApi.status);
+    console.log(responseApi.body)
+  })
+
+  it('WHEN search user registered with dynamic way', async()=> {
+    responseApi = await api.getUser('arief')
+    console.log("status code: ", responseApi.status)
+    console.log(responseApi.body)
+  })
+})
+
+describe('POST, /v1/users', async()=> {
+  it('WHEN registered user with valid data', async()=> {
+
   })
 })
