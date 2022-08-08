32e411fa04434834570c3fbac318db45b9a1ec15
aryandhi.a.w <aryandh.a.w@gmail.com>
2022-08-08 20:26:43 +0700

    GET automation

--- sesi5_apiAutomation/testcases/user-testcase.js
+++ sesi5_apiAutomation/testcases/user-testcase.js
@@ -1,27 +1,51 @@
+const expect = require('chai').expect;
 const api = require('../api/user-endpoint');
 
-describe('GET By Name, /v1/users', async()=> {
+describe('| GET By Name | /v1/users?name=', async()=> {
+  let responseApi;
+  let bodyData;
+
   it('WHEN searching data un-registered', async()=> {
-    let responseApi = await api.getUser('ridho');
-    console.log("status code: ", responseApi.status);
-    console.log(responseApi.body);
+    responseApi = await api.getUser('ridho');
+    bodyData = responseApi.body;
+    expect(responseApi.status).to.equal(200);
+    expect(bodyData.data.length).to.equal(0);
+
+    // console.log(bodyData);
+    // console.log("status code: ", responseApi.status);
+    // console.log(responseApi.body);
+    
   })
 
-  it('WHEN search user registered with static way', async()=> {
-    responseApi = await api.getUserStatic()
-    console.log("status code: ", responseApi.status);
-    console.log(responseApi.body)
-  })
+  // it('WHEN search user registered with static way', async()=> {
+  //   responseApi = await api.getUserStatic();
+  //   bodyData = responseApi.body;
+  //   expect(responseApi.status).to.equal(200);
+  //   expect(bodyData.data[0].firstName).to.equal('ramadhan');
 
-  it('WHEN search user registered with dynamic way', async()=> {
-    responseApi = await api.getUser('arief')
-    console.log("status code: ", responseApi.status)
-    console.log(responseApi.body)
-  })
-})
+  //   // expect(bodyData.data[0].firstName.toLowerCase()).to.equal('Ramadhan');
+  //   // console.log(bodyData.data[0].firstName);
+  //   // console.log("status code: ", responseApi.status);
+  //   // console.log(responseApi.body)
+  // })
 
-describe('POST, /v1/users', async()=> {
-  it('WHEN registered user with valid data', async()=> {
+  // it('WHEN search user registered with dynamic way', async()=> {
+  //   const paramsName = 'arief';
+  //   responseApi = await api.getUser(paramsName);
+  //   bodyData = responseApi.body;
+  //   expect(responseApi.status).to.equal(200);
+  //   expect(bodyData.data[0].firstName).to.equal(paramsName);
 
-  })
-})
+    
+  //   // expect(bodyData.data[0].firstName.toLowerCase()).to.equal('Arief');
+  //   // console.log("status code: ", responseApi.status);
+  //   // console.log(responseApi.body);
+  //   // console.log(bodyData.data[0].firstName);
+  // })
+});
+
+// describe('POST, /v1/users', async()=> {
+//   it('WHEN registered user with valid data', async()=> {
+
+//   })
+// })
